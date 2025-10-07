85ca1574252c95b6f72789225fcfceae6944e1e4
artvandelay <jigarkdoshi@gmail.com>
2025-10-08 04:55:24 +0530

    Update Google Analytics tracking ID to new account (G-44G6TK06ZN)

--- docs/javascripts/analytics.js
+++ docs/javascripts/analytics.js
@@ -1,11 +1,11 @@
 // Google tag (gtag.js) - Load the script first
 var script = document.createElement('script');
 script.async = true;
-script.src = 'https://www.googletagmanager.com/gtag/js?id=G-9JKSMSLNJ9';
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-44G6TK06ZN';
 document.head.appendChild(script);
 
 // Initialize gtag after script loads
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-9JKSMSLNJ9');
+gtag('config', 'G-44G6TK06ZN');
