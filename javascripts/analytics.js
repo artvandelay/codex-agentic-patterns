5408eed5581aa9f4e7eaebf3d5f4ac8659c672c3
artvandelay <jigarkdoshi@gmail.com>
2025-10-08 04:43:43 +0530

    Deployed b88df13 with MkDocs version: 1.6.1

--- javascripts/analytics.js
+++ javascripts/analytics.js
@@ -1,6 +1,11 @@
-// Google tag (gtag.js)
+// Google tag (gtag.js) - Load the script first
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-9JKSMSLNJ9';
+document.head.appendChild(script);
+
+// Initialize gtag after script loads
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-
 gtag('config', 'G-9JKSMSLNJ9');
